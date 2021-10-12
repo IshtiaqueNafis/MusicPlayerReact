00634573be47f73df76e8d8e225f635873d230ea
user.email <46496245+Fallofduty@users.noreply.github.com>
2021-10-12 06:23:14 -0400

    added song and player component

---
+++ src/components/song.jsx
@@ -0,0 +1,13 @@
+//shows song name artist and pictures
+
+import React from 'react';
+
+const Song = () => {
+    return (
+        <div className='song-container'>
+
+        </div>
+    );
+};
+
+export default Song;
