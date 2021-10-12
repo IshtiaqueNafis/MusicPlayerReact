46f43df4937ae64187da99debf197c476c8dc2e3
user.email <46496245+Fallofduty@users.noreply.github.com>
2021-10-12 07:00:40 -0400

    created utis file to hold all the songs, created usestate songs and currentsong and passed down current song on props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import './styles/App.scss'
 import Player from "./components/player";
 import Song from "./components/song";
+import data from './utlis'
+import {useState} from "react";
 
-const App = () => (
-    <div className="App">
-        <h1> Music </h1>
-        <Song/>
-        <Player/>
+const App = () => {
+    const [songs, setSongs] = useState(data())
+    const [currentSong, setCurrentSong] = useState(songs[0])
+    return (
 
-    </div>
-);
+        <div className="App">
+            <h1> Music </h1>
+            <Song currentSong={currentSong}/>
+            <Player/>
+
+        </div>
+    );
+};
 
 export default App;
